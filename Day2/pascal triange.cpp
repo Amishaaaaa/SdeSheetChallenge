111b3670b210713b3d9232add839d4b8fa1a669e
Amisha <amishakumari485@gmail.com>
2022-06-09 20:47:47 +0530

    Pascal's Triangle problem solved

--- Day2/pascal triange.cpp
+++ Day2/pascal triange.cpp
@@ -14,19 +14,3 @@ vector<vector<long long int>> printPascal(int n)
   }
     return v;
 }
-#include <bits/stdc++.h> 
-
-vector<vector<long long int>> printPascal(int n) 
-{
-  // Write your code her
-  vector<vector<long long int>>v(n);
-  for(int i=0;i<n;i++){
-      v[i].resize(i+1);
-      v[i][0]=v[i][i]=1;
-      
-      for(int j=1;j<i;j++){
-          v[i][j]=v[i-1][j-1]+v[i-1][j];
-      }
-  }
-    return v;
-}
